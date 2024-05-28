a84d7e02aa3f8f2ccadf2b11e16a6ad7c1d595a6
poojaagrawal1915 <https://github.com/poojaagrawal1915>
2024-05-28 18:43:55 +0530

    updating files

--- template.py
+++ template.py
@@ -17,7 +17,7 @@ list_of_files=[
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/artifact_entity.py",
-    f"{project_name}/entity/config_entity",
+    f"{project_name}/entity/config_entity.py",
     f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
     f"{project_name}/pipeline/__init__.py",
